e282593d0cc0a3170ebf09e3360eb63480683f41
clencyc <clency2023@gmail.com>
2024-10-01 05:23:48 +0000

    task 1 and 2

---
+++ task_1.java
@@ -0,0 +1,37 @@
+// Write a Java program to create a class called "Person" with a name and age attribute. Create two
+// instances of the "Person" class, set their attributes using the constructor, and print their name and
+// age.
+
+class Person {
+    String name;
+    int age;
+
+    // Constructors to initialize the attributes,
+    // they allow you to set initial values for
+    // the object's attributes and perform any setup required
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters to get the attributes
+    public String getNAme() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class task_1 {
+    public static void main(String[] args) {
+        // create two instances of person class
+        Person person1 = new Person("Alice", 35);
+        Person person2 = new Person("Bob", 25);
+
+        System.out.println("Name: " + person1.getNAme() + ", Age: " + person1.getAge());
+        System.out.println("Name: " + person2.getNAme() + ", Age: " + person2.getAge());
+
+    }
+}
---
+++ task_2.java
@@ -0,0 +1,34 @@
+// Write a Java program to create a class called "Dog" with a name and breed attribute. Create two
+// instances of the "Dog" class, set their attributes using the constructor and modify the attributes using
+// the setter methods and print the updated values.
+
+class Dog {
+    String name;
+    String breed;
+
+    // Constructors to initialize the attributes,
+    // they allow you to set initial values for
+    // the object's attributes and perform any setup required
+    Dog(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    // Getters to get the attributes
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    // Setters to set the attributes
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
